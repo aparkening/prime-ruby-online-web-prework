3dd471974983e5279756412e5a35e7fc79f73d7d
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 17:35:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,24 +1,24 @@
 # Take an integer and return boolean whether integer is a prime number
 def prime?(number)
   if number < 4
-    puts "Iterated once"
+    # puts "Iterated once"
     return number > 1
   elsif (number % 2 == 0 || number % 3 == 0)
-    puts "Iterated once"
+    # puts "Iterated once"
     return false
   else 
     checking_number = 5
-    iterated = 1
+    # iterated = 1
     while checking_number * checking_number <= number
       if (number % checking_number == 0 || number % (checking_number + 2) == 0)
-        puts "Iterated #{iterated} times"
+        # puts "Iterated #{iterated} times"
         return false
       else 
         checking_number = checking_number + 6
-        iterated += 1
+        # iterated += 1
       end
     end
-    puts "Iterated #{iterated} times"
+    # puts "Iterated #{iterated} times"
     return true
   end
 end
