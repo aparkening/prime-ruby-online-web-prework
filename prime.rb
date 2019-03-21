695ccf72a9ed31d34a46460dcd8dc6c67438b1f3
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 17:16:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(number)
   # Return true if number is 2 or 3
   if number <= 3
     return number > 1
-  elsif (number % 2 == 0 || number % 3 == 0)
-    return false
+  #elsif (number % 2 == 0 || number % 3 == 0)
+  #  return false
   else 
     checking_number = 5
     while checking_number * checking_number <= number
