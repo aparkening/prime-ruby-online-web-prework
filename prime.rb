9fa4244618a426005d0124bf8269db4df9ef9603
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 17:10:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -21,21 +21,5 @@ def prime?(number)
 end
 
 puts Benchmark.measure {
-def prime?(number)
-  if number <= 3
-    return number > 1
-  elsif (number % 2 == 0 || number % 3 == 0)
-    return false
-  else 
-    checking_number = 5
-    while checking_number * checking_number <= number
-      if (number % checking_number == 0 || number % (checking_number + 2) == 0)
-        return false
-      else 
-        checking_number = checking_number + 6
-      end
-    end
-    return true
-  end
-end
+  prime?(1763)
 }
