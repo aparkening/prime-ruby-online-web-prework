315da914e90f26cea32a153fdaf01a3cbfbba073
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 17:20:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 # Take an integer and return boolean whether integer is a prime number
 def prime?(number)
-  # Return true if number is 2 or 3
-  if number <= 4
+  if number < 4
     return number > 1 && !(number % 2 == 0 || number % 3 == 0)
-  #elsif (number % 2 == 0 || number % 3 == 0)
-  #  return false
+  elsif (number % 2 == 0 || number % 3 == 0)
+    return false
   else 
     checking_number = 5
     while checking_number * checking_number <= number
