f931c431dd66413bdbdd66b45d575cf9155788d4
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 16:02:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -10,9 +10,10 @@ def prime?(integer)
    function is_prime(n)
      if n <= 3
         return n > 1
-     else if n mod 2 = 0 or n mod 3 = 0
+     else if n % 2 = 0 or n % 3 = 0
         return false
      let i ← 5
+     
      while i * i ≤ n
         if n mod i = 0 or n mod (i + 2) = 0
             return false
