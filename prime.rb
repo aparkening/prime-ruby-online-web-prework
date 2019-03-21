88c3a6c18318e96bb893084d161211962b9fba5f
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 15:51:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,13 @@
 
 # Take an integer return boolean whether integer is a prime number
 def prime?(integer)
+  if 
+  divide by 2 and 
+  determine if whole number 
   
   
   
 end
+
+
+To prove whether a number is a prime number, first try dividing it by 2, and see if you get a whole number. If you do, it can't be a prime number. If you don't get a whole number, next try dividing it by prime numbers: 3, 5, 7, 11 (9 is divisible by 3) and so on, always dividing by a prime number (see table below).
